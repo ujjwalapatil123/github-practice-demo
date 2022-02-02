af95c7baed7b76c861a3ccd213fd0390e275a1cd
ujjwalapatil123 <98448243+ujjwalapatil123@users.noreply.github.com>
2022-02-02 19:46:10 +0530

    Implemented subtract function from remote repo

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ def add(x,y):
     return x+y   #on Bug123
     #substract implementation
 def substract(x,y):
-    pass
+    return x-y   #on remote repo
     #multiply implementation
 def multiply(x,y):
     pass
