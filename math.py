ec6f9d17c141b1464a57481a6db994eaa3b00553
ujjwalapatil123 <ujjwala_patil@persistent.com>
2022-02-02 16:37:36 +0530

    Added all fn() header

--- math.py
+++ math.py
@@ -1,9 +1,12 @@
-
+#Add implementation
 def add(x,y):
     pass
+    #substract implementation
 def substract(x,y):
     pass
+    #multiply implementation
 def multiply(x,y):
     pass
+    #divide implementation
 def divide(x,y):
     pass
